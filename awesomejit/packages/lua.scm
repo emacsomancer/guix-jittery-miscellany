3efda497d9e42a6dd177e02ed34d502eae4eb781
Benjamin Slade <slade@lambda-y.net>
2023-11-30 12:22:44 -0600

    add luajit master

---
+++ awesomejit/packages/lua.scm
@@ -0,0 +1,53 @@
+(define-module (awesomejit packages lua)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix utils)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages lua))
+
+(define-public luajit
+  (let ((branch "master")
+        (commit "43d0a19158ceabaa51b0462c1ebc97612b420a2e"))
+    (package
+      (name "luajit")
+      (version (git-version branch "0" commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://luajit.org/git/luajit.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1a002yh8v1i1q9w09494q0b8vsbmw3amn9jgfs5qnz7ba54jij0q"))))
+      (build-system gnu-build-system)
+      (arguments
+       `(#:tests? #f                    ; luajit is distributed without tests
+         #:phases
+         (modify-phases %standard-phases
+           (delete 'configure)          ; no configure script
+           (add-after 'install 'create-luajit-symlink
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let* ((out (assoc-ref outputs "out"))
+                      (bin (string-append out "/bin")))
+                 (with-directory-excursion bin
+                   (symlink ,(string-append name "-" branch)
+                            ,name))))))
+         #:make-flags (list (string-append "PREFIX="
+                                           (assoc-ref %outputs "out")))))
+      (home-page "https://www.luajit.org/")
+      (synopsis
+       "Just in time compiler for Lua programming language version 5.1")
+      ;; On powerpc64le-linux, the build fails with an error: "No support for
+      ;; PowerPC 64 bit mode (yet)".  See: https://issues.guix.gnu.org/49220
+      (supported-systems (fold delete %supported-systems
+                               (list "powerpc64le-linux" "riscv64-linux")))
+      (description
+       "LuaJIT is a Just-In-Time Compiler (JIT) for the Lua
+programming language.  Lua is a powerful, dynamic and light-weight programming
+language.  It may be embedded or used as a general-purpose, stand-alone
+language.")
+      (license license:x11))))
+

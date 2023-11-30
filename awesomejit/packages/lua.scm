4f206839600d23bef3830c6e3254863fa863f06e
Benjamin Slade <slade@lambda-y.net>
2023-11-30 14:25:37 -0600

    try github mirror

--- awesomejit/packages/lua.scm
+++ awesomejit/packages/lua.scm
@@ -17,7 +17,7 @@
       (source (origin
                 (method git-fetch)
                 (uri (git-reference
-                      (url "https://luajit.org/git/luajit.git")
+                      (url "https://github.com/LuaJIT/LuaJIT.git")
                       (commit commit)))
                 (file-name (git-file-name name version))
                 (sha256

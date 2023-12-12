49c3eb27ba4628e1316282c80ba93cf40bc60d3e
Benjamin Slade <slade@lambda-y.net>
2023-12-12 10:30:43 -0600

    change xscreensaver path

--- awesomejit/packages/xdisorg.scm
+++ awesomejit/packages/xdisorg.scm
@@ -171,7 +171,7 @@
        '("--with-pam-service-name=login"
          "--with-xkb"
          "--with-default-authproto-module=/run/setuid-programs/authproto_pam"
-         "--with-xscreensaver=/run/current-system/libexec/xscreensaver")))
+         "--with-xscreensaver=/run/current-system/profile/bin/xscreensaver")))
     (native-inputs
      (list pandoc pkg-config))
     (inputs

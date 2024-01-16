f4a6ee59715772b4b5c8c0fec0f03aa4a4abfa7b
Benjamin Slade <slade@lambda-y.net>
2024-01-16 09:52:19 -0600

    add lispe package

--- awesomejit/packages/lisp.scm
+++ awesomejit/packages/lisp.scm
@@ -103,6 +103,44 @@
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1))
 
+
+(define-public lispe
+  (let ((commit "3b5ffad81767ce63e2562f65a9e8a2a992952799") (revision "0"))
+    (package
+      (name "lispe")
+      (version (git-version nil revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/naver/lispe")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1flkgh524lq3024p7ld5lg743s1v7qnbmgv77578rzmn2rjzr77n"))))
+      (build-system cmake-build-system)
+      ;; (outputs '("out" "doc"))
+      ;; (arguments
+      ;;  `(#:phases (modify-phases %standard-phases
+      ;;               (add-after 'install 'move-doc
+      ;;                 (lambda* (#:key outputs #:allow-other-keys)
+      ;;                   (let* ((name ,(package-name argagg)) (out (assoc-ref
+      ;;                                                              outputs
+      ;;                                                              "out"))
+      ;;                          (doc (assoc-ref outputs "doc")))
+      ;;                     (mkdir-p (string-append doc "/share/doc"))
+      ;;                     (rename-file
+      ;;                      (string-append out "/share/doc/" name)
+      ;;                      (string-append doc "/share/doc/" name))))))))
+      ;; (native-inputs (list doxygen))
+      (home-page "https://github.com/naver/lispe")
+      (synopsis "C++11 command line argument parser")
+      (description
+       "An implementation of a full fledged Lisp interpreter with
+ Data Structure, Pattern Programming and High level Functions with
+ Lazy Evaluation à la Haskell. ")
+      (license license:bsd-3))))
+
 ;; (define-public sbcl
 ;;   (package
 ;;     (name "sbcl")

925268d1b24c78e7eb5d67b264947a5b245c5f4a
Benjamin Slade <slade@lambda-y.net>
2024-08-09 10:33:47 -0500

    update isync/mbsync hash

--- awesomejit/packages/mail.scm
+++ awesomejit/packages/mail.scm
@@ -210,7 +210,7 @@
        (uri (string-append "mirror://sourceforge/isync/isync/"
                            version "/isync-" version ".tar.gz"))
        (sha256 (base32
-                "1zq0wwvmqsl9y71546dr0aygzn9gjjfiw19hlcq87s929y4p6ckw"))
+                "13gpawspfpiw87wm2p6gzrm98xwy741k2ib1l6fjggw7jc81xj50"))
        (patches
         ;; Likely to be included in next version
         (search-patches "isync-openssl3-fix.patch"))))

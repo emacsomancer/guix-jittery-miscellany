a1fba6efd73bf3927344095902b33f12a0aaae41
Benjamin Slade <slade@lambda-y.net>
2024-08-09 10:26:55 -0500

    update isync/mbsync to 1.5.0

--- awesomejit/packages/mail.scm
+++ awesomejit/packages/mail.scm
@@ -200,6 +200,42 @@
   #:use-module (srfi srfi-1))
 
 
+(define-public isync
+  (package
+    (name "isync")
+    (version "1.5.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://sourceforge/isync/isync/"
+                           version "/isync-" version ".tar.gz"))
+       (sha256 (base32
+                "1zq0wwvmqsl9y71546dr0aygzn9gjjfiw19hlcq87s929y4p6ckw"))
+       (patches
+        ;; Likely to be included in next version
+        (search-patches "isync-openssl3-fix.patch"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+       #:phases
+       #~(modify-phases %standard-phases
+           (add-after 'install 'substitute-openssl-path
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (substitute* (string-append #$output "/bin/mbsync-get-cert")
+                 (("openssl s_client")
+                  (string-append (search-input-file inputs "/bin/openssl")
+                                 " s_client"))))))))
+    (native-inputs
+     (list perl))
+    (inputs
+     (list bdb cyrus-sasl openssl zlib))
+    (home-page "https://isync.sourceforge.io/")
+    (synopsis "Mailbox synchronization program")
+    (description
+     "isync/mbsync is a command-line tool for two-way synchronization of
+mailboxes.  Currently Maildir and IMAP are supported types.")
+    (license license:gpl2+)))
+
 ;; (define-public mu
 ;;   (package
 ;;     (name "mu")

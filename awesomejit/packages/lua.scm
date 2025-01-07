91239f1a3fa256be96f955f0d9436d4580835ac3
Benjamin Slade <slade@lambda-y.net>
2025-01-06 21:17:37 -0600

    new fennel version

--- awesomejit/packages/lua.scm
+++ awesomejit/packages/lua.scm
@@ -82,7 +82,7 @@ language.")
 (define-public fennel
   (package
     (name "fennel")
-    (version "1.5.0")
+    (version "1.5.1")
     (source (origin
               (method git-fetch)
               (uri (git-reference

e9ed4139dc9c376d20a55f622a8f9b623e87065a
Benjamin Slade <slade@lambda-y.net>
2025-01-14 17:02:04 -0600

    fix zen-twilight?

--- awesomejit/packages/zen-browser.scm
+++ awesomejit/packages/zen-browser.scm
@@ -177,7 +177,7 @@ We care about your experience, not your data.")
                                      (string-append #$(this-package-input "gtk+") "/share")
                                      (map
                                        (lambda (input)
-                                         (string-append (cdr input) "/lib"))
+                                         (string-append (cdr input) "/lib/zen-twilight"))
                                        inputs))
                                    ":")))
                       ;; Got this proc from hako's Rosenthal, thanks
@@ -193,12 +193,12 @@ We care about your experience, not your data.")
                         (append
                           (map
                             (lambda (binary)
-                              (string-append #$output "/lib/zen/" binary))
+                              (string-append #$output "/lib/zen-twilight/" binary))
                             '("glxtest" "updater" "vaapitest" "zen-twilight" "zen-twilight-bin" "pingsender"))
-                          (find-files (string-append #$output "/lib/zen") ".*\\.so.*"))))))
+                          (find-files (string-append #$output "/lib/zen-twilight/zen-twilight") ".*\\.so.*"))))))
                 (add-after 'patch-elf 'install-bin
                   (lambda _
-                    (let* ((zen (string-append #$output "/lib/zen/zen"))
+                    (let* ((zen (string-append #$output "/lib/zen-twilight/zen-twilight"))
                            (bin-zen (string-append #$output "/bin/zen-twilight")))
                       (mkdir (string-append #$output "/bin"))
                       (symlink zen bin-zen))))

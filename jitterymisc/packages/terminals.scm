7f76ebd28fb836d3ec7bfa645129979a7c25bd67
Benjamin Slade <emacsomancer@lambda-y.net>
2026-02-01 21:23:02 -0600

    add: alacritty-sixels

--- jitterymisc/packages/terminals.scm
+++ jitterymisc/packages/terminals.scm
@@ -133,7 +133,7 @@
   #:use-module (gnu packages xml)
   #:use-module (gnu packages xorg)
   #:use-module (gnu packages zig)
-  #:use-module (jitterymisc packages fonts)
+  ;; #:use-module (jitterymisc packages fonts)
   #:use-module (srfi srfi-26)
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages vulkan)
@@ -144,49 +144,273 @@
   #:use-module (guix packages)
   #:use-module (guix utils)
   ;; #:use-module (jitterymisc packages gtk)
-  ;; #:export (ghostty)
-  )
+  #:export (zutty)
+                                                       )
+
+(define-public alacritty-sixels
+  (package
+    (name "alacritty")
+    (version "0.16.1")
+    (source
+     (origin
+       ;; XXX: The crate at "crates.io" contains only the alacritty subproject
+       ;; of alacritty and thus has limited contents.  In particular,
+       ;; it does not contain "extra" directory with completions, icon, etc.
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/ayosec/alacritty")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "02gmz38akg41vfmhmh0d0vws89ayhyg0kchvxjzs1bpr5nw8q58j"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:install-source? #f
+       #:cargo-test-flags
+       '("--"
+         ;; Changes in clap regularly break this test.
+         "--skip=cli::tests::completions")
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'patch-xdg-open
+           (lambda* (#:key inputs #:allow-other-keys)
+             (substitute* "alacritty/src/config/ui_config.rs"
+               (("xdg-open") (search-input-file inputs "/bin/xdg-open")))))
+         (add-after 'configure 'add-absolute-library-references
+           (lambda* (#:key inputs vendor-dir #:allow-other-keys)
+             ;; Fix dlopen()ing some libraries on pure Wayland (no $DISPLAY):
+             ;; Failed to initialize any backend! Wayland status: NoWaylandLib
+             ;; XXX We patch transitive dependencies that aren't even direct
+             ;; inputs to this package, because of the way Guix's Rust build
+             ;; system currently works.  <http://issues.guix.gnu.org/46399>
+             ;; might fix this and allow patching them directly.
+             (substitute* (find-files vendor-dir "\\.rs$")
+               (("libEGL\\.so")
+                (search-input-file inputs "lib/libEGL.so"))
+               (("libGL\\.so")
+                (search-input-file inputs "lib/libGL.so"))
+               ;; Lots of libraries from rust-x11-dl and others.
+               (("libX[[:alpha:]]*\\.so" all)
+                (search-input-file inputs (string-append "lib/" all)))
+
+               ;; There are several libwayland libraries.
+               (("libwayland\\.so" all)
+                (search-input-file inputs (string-append "lib/" all)))
+               (("libwayland-[[:alpha:]]*\\.so" all)
+                (search-input-file inputs (string-append "lib/" all)))
+               (("libxkbcommon-x11\\.so")
+                (search-input-file inputs "lib/libxkbcommon-x11.so"))
+               (("libxkbcommon\\.so")
+                (search-input-file inputs "lib/libxkbcommon.so")))))
+         (replace 'install
+           ;; Upstream install script only takes care of executable.
+           (lambda* (#:key native-inputs inputs outputs #:allow-other-keys)
+             (let* ((out   (assoc-ref outputs "out"))
+                    (bin   (string-append out "/bin"))
+                    (share (string-append out "/share"))
+                    (icons (string-append share "/icons/hicolor/scalable/apps"))
+                    (tic   (search-input-file (or native-inputs inputs) "/bin/tic"))
+                    (man   (string-append share "/man"))
+                    (alacritty-bin (car (find-files "target" "^alacritty$"))))
+               ;; Install the executable.
+               (install-file alacritty-bin bin)
+               ;; Install man pages.
+               (mkdir-p (string-append man "/man1"))
+               (mkdir-p (string-append man "/man5"))
+               (define (create-manpage manpage)
+                 (let ((mandir (string-append
+                                 "/man" (string-take-right manpage 1) "/")))
+                   (with-input-from-file (string-append manpage ".scd")
+                     (lambda _
+                       (with-output-to-file (string-append man mandir manpage)
+                         (lambda _ (invoke "scdoc")))))))
+               (with-directory-excursion "extra/man"
+                 (for-each create-manpage '("alacritty.1"
+                                            "alacritty-msg.1"
+                                            "alacritty.5"
+                                            "alacritty-bindings.5")))
+               ;; Install desktop file.
+               (install-file "extra/linux/Alacritty.desktop"
+                             (string-append share "/applications"))
+               (install-file "extra/linux/org.alacritty.Alacritty.appdata.xml"
+                             (string-append share "/metainfo"))
+               ;; Install icon.
+               (mkdir-p icons)
+               (copy-file "extra/logo/alacritty-term.svg"
+                          (string-append icons "/Alacritty.svg"))
+               ;; Install terminfo.
+               (mkdir-p (string-append share "/terminfo"))
+               ;; We don't compile alacritty-common entry because
+               ;; it's being used only for inheritance.
+               (invoke tic "-x" "-e" "alacritty,alacritty-direct"
+                       "-o" (string-append share "/terminfo/")
+                       "extra/alacritty.info")
+               ;; Install completions.
+               (mkdir-p (string-append
+                         out "/share/bash-completion/completions"))
+               (copy-file "extra/completions/alacritty.bash"
+                          (string-append
+                           out "/share/bash-completion/completions/alacritty"))
+               (install-file "extra/completions/_alacritty"
+                             (string-append share "/zsh/site-functions"))
+               (install-file "extra/completions/alacritty.fish"
+                             (string-append share "/fish/vendor_completions.d"))))))))
+    (native-inputs
+     (list ncurses
+           pkg-config
+           python
+           scdoc))
+    (inputs
+     (cons* expat
+            fontconfig
+            freetype
+            libx11
+            libxcb
+            libxcursor
+            libxext
+            libxft
+            libxi
+            libxinerama
+            libxkbcommon
+            libxmu
+            libxpresent
+            libxrandr
+            libxscrnsaver
+            libxt
+            libxtst
+            libxxf86vm
+            mesa
+            xdg-utils
+            wayland
+            (cargo-inputs 'alacritty)))
+    (native-search-paths
+     ;; FIXME: This should only be located in 'ncurses'.  Nonetheless it is
+     ;; provided for usability reasons.  See <https://bugs.gnu.org/22138>.
+     (list (search-path-specification
+            (variable "TERMINFO_DIRS")
+            (files '("share/terminfo")))))
+    (home-page "https://alacritty.org/")
+    (synopsis "GPU-accelerated terminal emulator")
+    (description
+     "Alacritty is a GPU-accelerated terminal emulator with a strong focus on
+simplicity and performance.  With such a strong focus on performance, included
+features are carefully considered and you can always expect Alacritty to be
+blazingly fast.  By making sane choices for defaults, Alacritty requires no
+additional setup.  However, it does allow configuration of many aspects of the
+terminal.  Note that you need support for OpenGL 3.2 or higher. This
+branch supports display of sixels.")
+    (license license:asl2.0)))
+
 
 (define-public zutty
   (package
     (name "zutty")
     (version "0.16")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://git.hq.sig7.se/zutty.git")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "115skr3lcw0hdshgxl72qnh635ajy7kk07dnrh9fhbx1bhvqcm3k"))))
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://git.hq.sig7.se/zutty.git")
+                    (commit version)))
+             (sha256 (base32 "115skr3lcw0hdshgxl72qnh635ajy7kk07dnrh9fhbx1bhvqcm3k"))))
     (build-system waf-build-system)
+    (inputs (list pkg-config freetype glew libxmu libxt font-misc-misc))
     (arguments
      `(#:phases
        (modify-phases %standard-phases
-         (delete 'check)            ; no check
-       )))
-    ;; (propagated-inputs
-    ;;  (list libxmu))
-    (native-inputs
-     (list pkg-config python))
-    (inputs
-     (list freetype
-           glew
-           font-misc-misc ;; zutty has 9x18 font as default
-           libxt ;; needed for xmu to be detected by `pkg-config'
-           libxmu))
-    (home-page "https://tomscii.sig7.se/zutty/")
+                      (delete 'check))))
     (synopsis "X terminal emulator rendering through OpenGL ES Compute Shaders")
-    (description "Zutty [zuːc̟] is a GPU-accelerated terminal emulator for the X
+    (description
+     "Zutty [zuːc̟] is a GPU-accelerated terminal emulator for X
 Window, written in C++ and only relying on OpenGL ES 3.1 for rendering.
-What really sets Zutty apart is its radically simple, yet extremely efficient
-rendering implementation, coupled with a sufficiently complete (VTxxx) feature
-set to make it useful for a wide range of users. Zutty offers high throughput
-with low latency, and strives to conform to relevant (published or de-facto)
-standards. Zutty provides a clean implementation written from scratch, resulting
-in a minimal, maintainable, modern codebase unencumbered by historical baggage.")
+It provides efficient rendering, VTxxx support, and a minimal, maintainable codebase.
+The wrapper ensures Zutty finds its default font and falls back gracefully to X core fonts if needed.")
+    (home-page "https://tomscii.sig7.se/zutty/")
     (license license:gpl3)))
+
+;; (define-public zutty
+;;   (package
+;;     (name "zutty")
+;;     (version "0.16")
+;;     (source
+;;      (origin
+;;        (method git-fetch)
+;;        (uri (git-reference
+;;              (url "https://git.hq.sig7.se/zutty.git")
+;;              (commit version)))
+;;        (file-name (git-file-name name version))
+;;        (sha256 (base32 "115skr3lcw0hdshgxl72qnh635ajy7kk07dnrh9fhbx1bhvqcm3k"))))
+;;     (build-system waf-build-system)
+;;     (arguments
+;;      `(#:phases
+;;        (modify-phases %standard-phases
+;;          (delete 'check)
+;;          (add-after 'install 'wrap-zutty
+;;            (lambda* (#:key outputs #:allow-other-keys)
+;;              (let* ((out (assoc-ref outputs "out"))
+;;                     (font-dir (string-append out "/share/fonts")))
+;;                (mkdir-p font-dir)
+;;                ;; copy default 9x18 font
+;;                (install-file (string-append (package-input-path font-misc-misc) "/9x18.pcf")
+;;                              font-dir)
+;;                ;; wrap Zutty binary
+;;                (wrap-program (string-append out "/bin/zutty")
+;;                  (list (cons "FONT_PATH" font-dir)
+;;                        (cons "ZUTTY_FONT_FALLBACK" "9x18")))))))))
+;;     (native-inputs (list pkg-config python))
+;;     (inputs (list freetype glew libxmu libxt font-misc-misc))
+;;     (synopsis "X terminal emulator rendering through OpenGL ES Compute Shaders")
+;;     (description
+;;      "Zutty [zuːc̟] is a GPU-accelerated terminal emulator for X
+;; Window, written in C++ and only relying on OpenGL ES 3.1 for rendering.
+;; It provides efficient rendering, VTxxx support, and a minimal, maintainable codebase.
+;; The wrapper ensures Zutty finds its default font and falls back gracefully to X core fonts if needed.")
+;;     (home-page "https://tomscii.sig7.se/zutty/")
+;;     (license license:gpl3)))
+
+zutty
+;; (provide 'zutty)
+
+;; (define-public zutty
+;;   (package
+;;     (name "zutty")
+;;     (version "0.16")
+;;     (source
+;;      (origin
+;;        (method git-fetch)
+;;        (uri (git-reference
+;;              (url "https://git.hq.sig7.se/zutty.git")
+;;              (commit version)))
+;;        (file-name (git-file-name name version))
+;;        (sha256
+;;         (base32
+;;          "115skr3lcw0hdshgxl72qnh635ajy7kk07dnrh9fhbx1bhvqcm3k"))))
+;;     (build-system waf-build-system)
+;;     (arguments
+;;      `(#:phases
+;;        (modify-phases %standard-phases
+;;          (delete 'check)            ; no check
+;;        )))
+;;     ;; (propagated-inputs
+;;     ;;  (list libxmu))
+;;     (native-inputs
+;;      (list pkg-config python))
+;;     (inputs
+;;      (list freetype
+;;            glew
+;;            font-misc-misc ;; zutty has 9x18 font as default
+;;            libxmu
+;;            libxt ;; needed for xmu to be detected by `pkg-config'
+;;            ))
+;;     (home-page "https://tomscii.sig7.se/zutty/")
+;;     (synopsis "X terminal emulator rendering through OpenGL ES Compute Shaders")
+;;     (description "Zutty [zuːc̟] is a GPU-accelerated terminal emulator for the X
+;; Window, written in C++ and only relying on OpenGL ES 3.1 for rendering.
+;; What really sets Zutty apart is its radically simple, yet extremely efficient
+;; rendering implementation, coupled with a sufficiently complete (VTxxx) feature
+;; set to make it useful for a wide range of users. Zutty offers high throughput
+;; with low latency, and strives to conform to relevant (published or de-facto)
+;; standards. Zutty provides a clean implementation written from scratch, resulting
+;; in a minimal, maintainable, modern codebase unencumbered by historical baggage.")
+;;     (license license:gpl3)))
 
 

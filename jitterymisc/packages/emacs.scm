80b2125cc23b8887667e60eb48b5622a4fb43958
Benjamin Slade <slade@lambda-y.net>
2026-08-30 23:49:33 -0500

    further updates to Lucid/Athena Emacs v31.1

--- jitterymisc/packages/emacs.scm
+++ jitterymisc/packages/emacs.scm
@@ -271,6 +271,13 @@
                                        "emacs-native-comp-fix-filenames.patch"
                                        "emacs-native-comp-pin-packages.patch"
                                        "emacs-zoom-image-test-fix.patch"))
+              (patches (search-patches "emacs-disable-jit-compilation.patch"
+                                       "emacs-exec-path.patch"
+                                       "emacs-fix-scheme-indent-function.patch"
+                                       "emacs-native-comp-driver-options.patch"
+                                       "emacs-native-comp-fix-filenames.patch"
+                                       "emacs-native-comp-pin-packages.patch"
+                                       "emacs-zoom-image-test-fix.patch"))
               (modules '((guix build utils)))
               (snippet
                '(with-directory-excursion "lisp"
@@ -293,8 +300,30 @@
   (package
     (inherit emacs-lucid)
     (name "emacs-lucid-tune-cflags")
-    
-    (synopsis "Emacs text editor with Lucid toolkit and CFLAGS tuning.")
+    (version "31.1")
+    (version "31.1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/emacs/emacs-"
+                                  version ".tar.xz"))
+              (sha256
+               (base32
+                "1nggbgnns7lvxn68gzlcsgwh3bigvrbn45kh6dqia9yxlqc6zwxk"))
+              (patches (search-patches "emacs-disable-jit-compilation.patch"
+                                       "emacs-exec-path.patch"
+                                       "emacs-fix-scheme-indent-function.patch"
+                                       "emacs-native-comp-driver-options.patch"
+                                       "emacs-native-comp-fix-filenames.patch"
+                                       "emacs-native-comp-pin-packages.patch"
+                                       "emacs-zoom-image-test-fix.patch"))
+              (patches (search-patches "emacs-disable-jit-compilation.patch"
+                                       "emacs-exec-path.patch"
+                                       "emacs-fix-scheme-indent-function.patch"
+                                       "emacs-native-comp-driver-options.patch"
+                                       "emacs-native-comp-fix-filenames.patch"
+                                       "emacs-native-comp-pin-packages.patch"
+                                       "emacs-zoom-image-test-fix.patch"))
+    (synopsis "Emacs text editor with Lucid/Athena toolkit with CFLAGS tuning.")
     (arguments
      (substitute-keyword-arguments (package-arguments emacs-lucid)
        ((#:configure-flags flags #~'())
